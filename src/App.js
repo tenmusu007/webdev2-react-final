c394c37ad8cf43ce14d9a03c292bd0b060b9c344
Atsuya Tanaka <tenmusu007@gmail.com>
2022-09-05 19:52:40 -0700

    mereged test and main

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Nav from "./Components/nav/Nav";
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import Recipe from "./pages/Recipe";
 import Shopping from "./pages/Shopping";
+import HeroPage from "./Components/HeroPage/HeroPage";
 
 function App() {
 	return (
@@ -18,8 +19,9 @@ function App() {
 			<BrowserRouter>
 				<AuthContextProvider>
 					<DataProvider>
-						<Routes>
-							<Route path='/' element={<Recipe />} />
+            <Routes>
+							<Route path='/' element={<HeroPage/>} />
+							<Route path='/Recipe' element={<Recipe />} />
 							<Route path='MyshoppingList' element={<Shopping />} />
 						</Routes>
 					</DataProvider>
